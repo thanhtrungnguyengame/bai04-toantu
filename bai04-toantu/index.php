0bdd8e97c87fec734fe1e637627f32512ad0d9de
thanhtrungnguyengame <105788618+thanhtrungnguyengame@users.noreply.github.com>
2022-05-18 20:51:35 +0700

    Add files via upload

---
+++ bai04-toantu/index.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 5; 
+    $b = 3;
+    $ss_bang = $a == $b;
+    echo "so sanh: $ss_bang <br>";
+
+   $ss_khac = $a != $b;
+   echo "so sanh khac: $ss_khac <br>";
+   
+   $ss_lon_hon = $a > $b;
+   echo "so sanh lon hon:"
+?>
